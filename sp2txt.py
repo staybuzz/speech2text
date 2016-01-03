d4578c7af2dc39af43afbd344b5e74742df990fc
Yuuki Mori <wrong.int1109@gmail.com>
2016-01-04 00:01:43 +0900

    引数にファイル名を指定できるようにした。標準入力にも対応させたのでパイプを通じて利用可能

--- sp2txt.py
+++ sp2txt.py
@@ -1,6 +1,7 @@
 #coding: utf-8
 
 import speech_recognition as sr
+import sys
 
 class sp2txt:
     def __init__(self):
@@ -9,9 +10,7 @@ class sp2txt:
   
     def read_wave(self, wav):
         """ Open Wave Sound """
-        #with sr.WavFile("test.wav") as source:
         with sr.WavFile(wav) as source:
-            #self.audio = self.recognizer.record(source)
             return self.recognizer.record(source)
     
     def translate(self, audio):
@@ -27,5 +26,15 @@ class sp2txt:
 
 if __name__ == '__main__':
     r = sp2txt()
-    audio = r.read_wave("test.wav")
-    print r.translate(audio)
+
+    argvs = sys.argv
+    argc = len(argvs)
+    if argc == 1:
+        filepath = sys.stdin.read().splitlines()
+    else:
+        del argvs[0]
+        filepath = argvs
+
+    for filename in filepath:
+        audio = r.read_wave(filename)
+        print r.translate(audio)
